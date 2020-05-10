0ce8e85ef34b2f59e5abed12978aea782c749384
Kevi0128 <kevi0128@qq.com>
2020-05-10 22:42:19 +0800

    Initial commit

---
+++ src/main/java/com/kevi/code/WhoIsKeviApplication.java
@@ -0,0 +1,13 @@
+package com.kevi.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoIsKeviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhoIsKeviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kevi/code/WhoIsKeviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevi.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoIsKeviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
